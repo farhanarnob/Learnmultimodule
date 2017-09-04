b6894b2529a69b59cb08b346ac197d2ae158deb4
Farhan Rahman Arnob <farhanarnob@live.com>
2017-09-04 13:46:12 +0600

    where to call share preference value, onStart or onResume? onStart.

--- app/src/main/java/me/farhanarnob/learnmultimodule/MainActivity.java
+++ app/src/main/java/me/farhanarnob/learnmultimodule/MainActivity.java
@@ -44,8 +44,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
         setLocationValue();
     }
 
