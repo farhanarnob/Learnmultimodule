89db370779b0afaef3d04bfaf6aba642f1c4c6aa
Farhan Rahman Arnob <farhanarnob@live.com>
2017-09-06 23:20:38 +0600

    Share Intent

---
+++ app/src/main/java/me/farhanarnob/learnmultimodule/DemeReceiver.java
@@ -0,0 +1,16 @@
+package me.farhanarnob.learnmultimodule;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ${farhanarnob} on ${06-Oct-16}.
+ */
+
+public class DemeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/me/farhanarnob/learnmultimodule/MainActivity.java
+++ app/src/main/java/me/farhanarnob/learnmultimodule/MainActivity.java
@@ -2,9 +2,12 @@ package me.farhanarnob.learnmultimodule;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +17,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +40,36 @@ public class MainActivity extends AppCompatActivity {
         settings_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent newIntent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(newIntent);
+//                Intent newIntent = new Intent(MainActivity.this, SettingsActivity.class);
+//                startActivity(newIntent);
+                String location = PreferenceManager.getDefaultSharedPreferences(MainActivity.this)
+                        .getString(getString(R.string.pref_location_key),
+                                getString(R.string.pref_location_default));
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+                Uri geoLocation = Uri.parse("geo:0,0").buildUpon()
+                        .appendQueryParameter("q", location)
+                        .build();
+                mapIntent.setData(geoLocation);
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed");
+                }
             }
         });
         setLocationValue();
 
+        final Button shareButton = (Button) findViewById(R.id.share);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Hello");
+                startActivity(Intent.createChooser(shareIntent, "Share Using"));
+            }
+        });
 
     }
 
@@ -58,4 +88,5 @@ public class MainActivity extends AppCompatActivity {
         TextView newText = (TextView) findViewById(R.id.text_view);
         newText.setText(location);
     }
+
 }
